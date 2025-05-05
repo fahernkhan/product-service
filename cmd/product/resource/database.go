7ac24974ac97158b55f6ba58204d3c50da5ebc90
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-05-05 20:04:23 +0700

    add resource db

--- cmd/product/resource/database.go
+++ cmd/product/resource/database.go
@@ -1,1 +1,27 @@
 package resource
+
+import (
+	"fmt"
+	"log"
+	"product-service/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func InitDB(cfg *config.Config) *gorm.DB {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s passsword=%s dbname=%s sslmode=disable",
+		cfg.Database.Host, cfg.Database.Port, cfg.Database.User, cfg.Database.Password, cfg.Database.Name)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // for testing purpose only
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+
+	log.Println("Connected to DB")
+	return db
+}
